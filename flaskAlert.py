e3549c95bfdf6eaad331c1feade52cd837ae61e3
sajad <sajad@Snapps-MacBook-Pro-3.local>
2019-12-03 17:17:35 +0330

    remove telegram bot

--- flaskAlert.py
+++ flaskAlert.py
@@ -1,19 +1,9 @@
-import telegram
 import logging
 import json
 from flask import Flask
 from flask import request
-from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
-app.secret_key = 'aYT>.L$kk2h>!'
-app.config['BASIC_AUTH_USERNAME'] = 'XXXUSERNAME'
-app.config['BASIC_AUTH_PASSWORD'] = 'XXXPASSWORD'
-
-basic_auth = BasicAuth(app)
-app.config['BASIC_AUTH_FORCE'] = True
-bot = telegram.Bot(token="botToken")
-chatID = "-xchatIDx"
 
 @app.route('/alert', methods = ['POST'])
 def postAlertmanager():
@@ -40,10 +30,10 @@ Alertname: """+alert['labels']['alertname']+"""
 Instance: """+alert['labels']['instance']+"""
 """+alert['annotations']['description']+"""
 """
-            bot.sendMessage(chat_id=chatID,text=message)
+            print(message)
             return "Alert OK", 200
     except:
-        bot.sendMessage(chat_id=chatID,text="Failed to send via Flask to Telegram!")
+        print("Failed to send via Flask to Telegram!")
         return "Alert nOK", 200
 
 if __name__ == '__main__':
